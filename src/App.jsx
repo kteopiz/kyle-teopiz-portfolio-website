20c4411c64e61685cbafaee327958b1299109276
Kyle Teopiz <kylecteopiz@gmail.com>
2024-01-31 23:00:39 -0500

    Create baseline for header section Add Header and HeaderButton Components Begin styling for both of them

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,33 @@ import { useState } from 'react'
 import './App.css'
 
 export const App = () => {
-  return (
-    <h1>Hello World!</h1>
-  )
+  return <>
+    <Header />
+  </>
   }
 
 // Header Component to be re-used on multiple webpages in the future
+  // NOTES on HEADER: Change "Home" Button tab to an ICON
 const Header = () => {
-    
+  return <>
+    <div className='header-bar-container'>
+      <ul className='header-bar-left'>
+        <HeaderButton linkToPage={""} pageName={"Home"} /> 
+        <HeaderButton linkToPage={""} pageName={"Projects"} />
+        <HeaderButton linkToPage={""} pageName={"Experience"} />
+        <HeaderButton linkToPage={""} pageName={"Skills and Tools"} />
+      </ul>
+      <ul className='header-bar-right'>
+        <div>time and date filler</div>
+      </ul>
+    </div>
+  </>
 }
 
-f
+const HeaderButton = ({ linkToPage, pageName }) => {
+    return <>
+      <li className='header-button'><a className='header-link' href={linkToPage}>{pageName}</a></li>
+    </>
+}
+
+export default App
