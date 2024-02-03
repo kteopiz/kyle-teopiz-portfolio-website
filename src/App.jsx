85c41fc18aaf656bf66f83e31382c2fb0a105e15
Kyle Teopiz <kylecteopiz@gmail.com>
2024-02-03 01:04:16 -0500

    Experiment with structure of HomeBodySection Thinkning of making list items and images which appear state based Believe I should lift state to the HomeBodySection common ancestor The state there can access specific images URLs and li text messages requires some thinking and set up current thought process: hashmap, pre-configured imgs and text lines

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import './App.css'
 export const App = () => {
   return <>
     <Header />
+    <HomePageBody />
   </>
   }
 
 // Header Component to be re-used on multiple webpages in the future
   // NOTES on HEADER: Change "Home" Button tab to an ICON
-const Header = () => {
+export const Header = () => {
   const [scrolled, setScrolled] = useState(false);
 
   // Checks if the Document has scrolled past the Header NavBar, if so make it sticky and stay at the top of the page
   const handleScroll = () => {
-
     /* 
       Keep Track of:
       1) The header by ID
@@ -42,9 +42,10 @@ const Header = () => {
         <HeaderButton linkToPage={""} pageName={"Projects"} />
         <HeaderButton linkToPage={""} pageName={"Experience"} />
         <HeaderButton linkToPage={""} pageName={"Skills and Tools"} />
+        <HeaderButton linkToPage={""} pageName={"Contact Me"} />
       </ul>
       <ul className='header-bar-right'>
-        <div>time and date filler</div>
+        <div className='incomplete '>time and date filler</div>
       </ul>
     </div>
   </>
@@ -54,6 +55,26 @@ const HeaderButton = ({ linkToPage, pageName }) => {
     return <>
       <li className='header-button'><a className='header-link' href={linkToPage}>{pageName}</a></li>
     </>
+}
+
+const HomePageBody = () => {
+  return <>
+    <section className='hpb-container'>
+      <div className='hpb-text-section'>
+        <h3>Hey there, thanks for coming</h3>
+        <h1>I'm Kyle</h1>
+        <p>Welcome to my site that I feel embodies who I am and what I can do.</p>
+        <uL>
+          <li>I am a coffee enthusiast</li>
+          <li>Passionate software development nerd</li>
+          <li>A CS student getting better day by day</li>
+        </uL>
+      </div>
+      <div className='hpb-image-section'>
+        <h1>filler</h1>
+      </div>
+    </section>
+  </>
 }
 
 export default App
