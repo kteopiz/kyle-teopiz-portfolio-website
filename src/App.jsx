bc827ccc0144d39e8222daa57b39e8bceb5a3ac0
Kyle Teopiz <kylecteopiz@gmail.com>
2024-02-04 23:04:29 -0500

    Implement first iteration of Dynamic list item and image Made 2 new Componenets DynamicListItem and DynamicImage Both use the state TraitDisplay which is an array Index 0 is text for ListItem and Index 1 is an ImageURL to a pic in assets For now, only one list item and one corresponding picture

--- src/App.jsx
+++ src/App.jsx
@@ -58,6 +58,33 @@ const HeaderButton = ({ linkToPage, pageName }) => {
 }
 
 const HomePageBody = () => {
+  const [traitDisplay, setTraitDisplay] = useState([null, null])
+
+  // On Mount of the website we want to select a random state for our Trait Display
+  useEffect(() => {
+    handleTraitDisplay();
+  }, []);
+
+  const handleTraitDisplay = () => {
+    let traits = [
+      {
+        "text" : "I am a coffee enthusiast",
+        "imageURL" : "src/assets/dog2.png"
+      },
+
+      {
+        "text" : "Nerd on the Intersection of Environmental Impact and SDE",
+        "imageURL" : "src/assets/dog3.png"
+      }
+    ];
+
+    const randomTrait = traits[Math.floor(Math.random() * traits.length)];
+    const newTraitDisplayText = randomTrait.text;
+    const newTraitDisplayImageURL = randomTrait.imageURL;
+
+    setTraitDisplay([newTraitDisplayText, newTraitDisplayImageURL]);
+  }
+
   return <>
     <section className='hpb-container'>
       <div className='hpb-text-section'>
@@ -65,16 +92,29 @@ const HomePageBody = () => {
         <h1>I'm Kyle</h1>
         <p>Welcome to my site that I feel embodies who I am and what I can do.</p>
         <uL>
-          <li>I am a coffee enthusiast</li>
-          <li>Passionate software development nerd</li>
-          <li>A CS student getting better day by day</li>
+          <DynamicListItem text={traitDisplay[0]} />
         </uL>
+        <button>shuffle</button>
       </div>
       <div className='hpb-image-section'>
         <h1>filler</h1>
+        <DynamicImage imageURL={traitDisplay[1]}/>
       </div>
     </section>
   </>
+}
+
+const DynamicListItem = ({ text }) => {
+  return <>
+    <li>{text}</li>
+  </>
+}
+
+const DynamicImage = ({ imageURL }) => {
+  return <>
+      <img src={imageURL}></img>
+  </>
+
 }
 
 export default App
